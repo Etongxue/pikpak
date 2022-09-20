ba8f2705e8e762560c125e57cea16c2f2de60853
Etongxue <1341361765@qq.com>
2022-09-21 03:03:06 +0800

    yh

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig(({mode}) => {
   console.log(mode)
   return {
     base: mode === 'development' ? '' : '/pikpak',
+    build:{
+      chunkSizeWarningLimit: 1500,
+  //其他配置这里就省略了 减少点重复代码长度
+    }   
     plugins: [vue()],
     server: {
       proxy: {
