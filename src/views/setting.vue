c2bc3bace5d9bcdcdec4f7347ad69da96c465067
Etongxue <1341361765@qq.com>
2022-09-21 04:29:54 +0800

    pik

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -2,16 +2,14 @@
   <div class="list-page">
     <n-collapse :default-expanded-names="['-1', '0', '2', '3','4']">
       <n-collapse-item name="-1" >
-        <template #header>
-          绑定telegram   <a @click.stop="" href="https://www.tjsky.net/?p=220#Telegram" target="_blank"> <n-icon style="vertical-align: middle;" size="20" color="#d03050"><zoom-question></zoom-question></n-icon> </a>
-        </template>
+        <template #header> 绑定telegram </template>
         <n-input v-model:value="telegramUrl" placeholder="复制telegram绑定链接到这"></n-input>
         <p></p>
         <n-button :disabled="!telegramUrl" type="primary" @click="goTelegram">Telegram绑定</n-button>
         <a href="https://t.me/PikPak_Bot" target="_blank">Telegram机器人地址</a>
       </n-collapse-item>
       <n-collapse-item name="0" title="aria2设置">
-        <template #header>aria2设置   <a @click.stop="" href="https://www.tjsky.net/?p=220#arai2" target="_blank"> <n-icon style="vertical-align: middle;" size="20" color="#d03050"><zoom-question></zoom-question></n-icon> </a></template>
+        <template #header>aria2设置</template>
         <n-form label-width="100px" label-align="left" label-placement="left">
           <n-form-item label="aria2链接：">
             <n-input v-model:value="aria2Data.host" placeholder="例如http://localhost:6800/jsonrpc"></n-input>
@@ -37,7 +35,7 @@
         </n-form>
       </n-collapse-item>
       <n-collapse-item name="1" title="自动登录设置">
-        <template #header>自动登录设置   <a @click.stop="" href="https://www.tjsky.net/?p=220#i-6" target="_blank"> <n-icon style="vertical-align: middle;" size="20" color="#d03050"><zoom-question></zoom-question></n-icon> </a></template>
+        <template #header>自动登录设置</template>
         <n-form label-width="100px" label-align="left" label-placement="left">
           <n-form-item label="是否开启">
             <n-switch v-model:value="loginSwitch"></n-switch>
@@ -64,18 +62,7 @@
       <n-collapse-item title="关于" name="2">
         <n-space>
           <a href="https://mypikpak.com/" target="_blank" class="n-button">官方网站</a>
-          <a href="https://t.me/pikpak_userservice" target="_blank" class="n-button">官方交流群</a>
-          <a href="https://github.com/tjsky/pikpak" target="_blank" class="n-button">开源仓库</a>
-          <a href="https://www.tjsky.net/?p=201" target="_blank" class="n-button">部署教程</a>
           <a href="https://t.me/pikpak_userservice" target="_blank">问题反馈</a>
-        </n-space>
-        <br />
-      </n-collapse-item>
-       <n-collapse-item title="PC端IDM支持" name="4">
-        <n-space>
-          <a href="https://github.com/MotooriKashin/ef2/releases" target="_blank" class="n-button">地址一：下载支持插件</a>
-          <a href="https://url71.ctfile.com/f/21226171-531688310-489b35" target="_blank" class="n-button">地址二：下载支持插件（密码pikpak）</a>
-          <a href="https://www.tjsky.net/?p=220#PCIDM" target="_blank" class="n-button">使用教程</a><br>先用第一个下载地址，如果你实在无法访问github再使用第二个下载地址，调用IDM下载需要配合“自定义菜单”功能实现，请查看上方的使用教程        
         </n-space>
         <br />
       </n-collapse-item>
@@ -90,8 +77,7 @@
 import { ref } from '@vue/reactivity';
 import { onMounted } from '@vue/runtime-core';
 import http from '../utils/axios'
-import { NForm, NFormItem, NButton, NInput, NCollapse, NCollapseItem, NSpace, NSwitch, useDialog, NAlert, NLog, NIcon } from 'naive-ui'
-import { ZoomQuestion } from '@vicons/tabler'
+import {  useDialog } from 'naive-ui'
 import {proxy as proxyDefault} from '../config'
 const logs = ref([
   '手机注册登陆',
