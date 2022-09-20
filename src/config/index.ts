6e5698bd4e230ebf296f84feb104e8e659c59453
Etongxue <1341361765@qq.com>
2022-09-21 01:54:43 +0800

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+export const proxy = [
+  'https://diii.tk',
+  'https://api-pikpak.tjsky.cf',
+  'https://api-pikpak.tjsky-1.cf',
+  'https://pikpak.he13797739994.ga',
+  'https://pikpak.hdddd.me',
+  'https://pan.yooyi.tk',
+  'https://pikpak.nsso.eu.org',
+  'https://pikpak-web.wnbh.cf',
+  'https://pk.pikpakz.top',
+  'https://cc123.cf',
+  'https://cf-api.xswd.cf',
+]
+
+export const version = '1.23.0'
